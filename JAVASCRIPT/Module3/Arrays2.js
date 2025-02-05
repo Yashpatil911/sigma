07c864199f3b0f8eeceebef95fa7197263401878
Yashpatil911 <yashjiu@gmail.com>
2025-02-05 09:55:51 +0530

    Add files via upload

---
+++ JAVASCRIPT/Module3/Arrays2.js
@@ -0,0 +1,27 @@
+let primary = ["red","yelow","blue"]
+let secondary = ["orange","green","violet"]
+
+// console.log(primary.concat(secondary));
+// console.log(secondary.concat(primary));
+// concat method creates new array so original(primmary,secandary) array is same(unaffected)
+// console.log(primary);
+// console.log(secondary);
+
+// reverse method changes original array
+// console.log(primary.reverse());
+// console.log(primary)
+
+let cars = ['audi','bmw','xuv','maruti']
+// console.log(cars.slice())
+// console.log(cars.slice(1))
+// console.log(cars.slice(1,3))
+// console.log(cars.slice(3))
+// console.log(cars.slice(cars.length-1))
+// console.log(cars.slice(5))
+// console.log(cars.slice(cars.length))
+
+console.log(cars.slice(-1));
+console.log(cars.slice(-2));
+console.log(cars.slice(-3));
+console.log(cars.slice(-4));
+console.log(cars.slice(-6));
